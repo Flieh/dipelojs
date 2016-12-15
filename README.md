cc058811bf122b544e188935a5973ecc02c7e100
Flieh <ruedelaflieh@gmail.com>
2016-12-15 08:30:30 +0100

    up and running

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dipelojs
+
+This is a port of my original Dipelo project written in PHP.
+
--- js/server.js
+++ js/server.js
@@ -1,5 +1,12 @@
-(function main () {
+$.getJSON( "./json/data.json", function( data ) {
+  var count = 0;
+  var sortedData = _.orderBy(data, "lastGamePlayed", "desc");
+  sortedData.forEach( function(element) {
+    count += 1;
+     for (var key in element) {
+       $('#content').append(key + ':   ' + element[key] + '<br>');
+     }
+  });
+});
 
-  $('#main').html('<h1>Hello World</h1>');
 
-})();
