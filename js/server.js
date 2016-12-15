b4582f16cd060b52299c96d6a317fc8d04b91610
Flieh <ruedelaflieh@gmail.com>
2016-12-15 18:44:39 +0100

    templates from separate file

--- js/server.js
+++ js/server.js
@@ -1,4 +1,10 @@
-$.getJSON( "./json/gameList.json", function( data ) {
+var context = {title: "My New Post", body: "This is my first post!"};
+$.get('templates/tmplt.hbs', function (data) {
+    var template=Handlebars.compile(data);
+    $('#display').html(template(context));
+}, 'html')
+
+/*$.getJSON( "./json/gameList.json", function( data ) {
   var count = 0;
   var sortedData = _.orderBy(data, ["gameFinished","gameNumber"],["asc","asc"]);
   sortedData.forEach( function(element) {
@@ -8,5 +14,5 @@ $.getJSON( "./json/gameList.json", function( data ) {
      }
   });
 });
-
+*/
 
