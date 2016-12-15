61af700c7891fa03f8f0d233038d7a5af2d2c6e0
Flieh <ruedelaflieh@gmail.com>
2016-12-15 18:09:11 +0100

    sorting with lodash

--- js/server.js
+++ js/server.js
@@ -1,6 +1,6 @@
-$.getJSON( "./json/data.json", function( data ) {
+$.getJSON( "./json/gameList.json", function( data ) {
   var count = 0;
-  var sortedData = _.orderBy(data, "lastGamePlayed", "desc");
+  var sortedData = _.orderBy(data, ["gameFinished","gameNumber"],["asc","asc"]);
   sortedData.forEach( function(element) {
     count += 1;
      for (var key in element) {
