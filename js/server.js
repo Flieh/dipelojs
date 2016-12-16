cc0727e854009788e45d3133c210e210b08f8b78
Flieh <ruedelaflieh@gmail.com>
2016-12-16 20:48:02 +0100

    player names truncated

--- js/server.js
+++ js/server.js
@@ -2,8 +2,14 @@
 $.getJSON( "./json/playerStats.json", function(data) {
     var sortedData = _.orderBy(data,"playerRating","desc");
     var context = {playerStats: sortedData};
+
     $.get('templates/tmplt.hbs', function (template) {
-    var compiledTemplate=Handlebars.compile(template);
-    $('#display').html(compiledTemplate(context));
-  }, 'html');
+      var compiledTemplate=Handlebars.compile(template);
+      $('#display').html(compiledTemplate(context));
+    }, 'html');
+});
+
+Handlebars.registerHelper('truncEmail', function(string) {
+  var emailParts =  string.split('@');
+  return emailParts[0];
 });
